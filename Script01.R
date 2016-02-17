9ede99c9d8142127a31e0e4b70e0e077bca9334e
Diego Paul Huaraca S <diego.21.phs@hotmail.com>
2016-02-17 16:18:24 -0500

    Update Pagos

--- Script01.R
+++ Script01.R
@@ -26,8 +26,44 @@ simmul <- function(ta, mc, t){
      return(vec)
 }
 
+ind <- function(ta, pij, mc, t){
+     val <- ifelse(simprod(ta, pij, t) - simmul(ta, mc, t)>0,1,0)
+     return(val)
+}
+
 
 # Simulacion
 plot(simprod(0.04, 0.035, 400), col="green", ylim=c(0,25), xlim=c(0,400), type='l', lwd=2, main="Simulación", ylab='')
 par(new=TRUE)
 plot(simmul(0.04, 0.0933, 400), col="red", ylim=c(0,25), xlim=c(0,400), type='l', lwd=2, main="Simulación", ylab='')
+
+ind(0.04, 0.035, 0.0933, 400)
+
+## Responsabilidad
+
+ResPat <- function(xt,t){
+     val <- rep(xt, t)*simprod(0.04, 0.035, t)*ind(0.04, 0.035, 0.0933, t) - rep(xt, t)*simmul(0.04, 0.0933, t)*ind(0.04, 0.035, 0.0933, t)
+     return(max(sum(val), 25*366))
+}
+
+ResPat(100,180)
+
+Rsup <- sapply(seq(180,400), function(i){ResPat(25,i)})
+
+
+Res <- function(xt, t){
+     sxt <- xt*t
+     r <- 2*(0.0764/12)
+     res <- t*sxt*r + 0.5*(366)
+     return(res)
+}
+
+Rinf <- sapply(seq(1,179), function(i){Res(25,i)})
+
+# Curvas pagos
+plot(cbind(seq(180,400), Rsup), xlim=c(0,400), ylim=c(0,30000), ylab='Valor', col="green", main='Aporte 25')
+par(new=TRUE)
+plot(cbind(seq(1,179), Rinf), xlim=c(0,400), ylim=c(0,30000), ylab='Valor', col="red", main='Aporte 25')
+
+
+plot(cbind(seq(1,179), Rinf), xlim=c(0,180), ylim=c(0,1000), ylab='Valor', col="red")
