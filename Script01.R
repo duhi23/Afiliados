0a288f26c8c552047d997a6f3c663fbef7f75611
Diego Paul Huaraca S <diego.21.phs@hotmail.com>
2016-02-17 08:39:18 -0500

    Update Script

--- Script01.R
+++ Script01.R
@@ -2,38 +2,32 @@
 #####     Responsabilidad Patronal     #####
 ############################################
 
-# Funciones basicas
-prod <- function(ta, pij, t, k){
-mult<-1	
-	for(i in k:t){
-		mult <- mult*(((1+ta)*(1+pij))^(1/12))
-	}
-return(mult)
+prod <- function(ta, pij, k){
+     mult<-1
+     for(j in 1:k){
+          mult <- mult*((1+pij)*(1+ta))^(1/12)
+     }
+     return(mult)
 }
 
 simprod <- function(ta, pij, t){
-	vec <- vector(length=t)
-	for(j in 1:t){
-		vec[j] <- prod(ta, pij, t, j)
-	}
-	return(vec)
+     vec <- vector(length=t)
+     for(k in 1:t){
+          vec[k] <- prod(ta, pij, k)
+     }
+     return(vec)
 }
 
 simmul <- function(ta, mc, t){
-	vec <- vector(length=t)
-	for(k in 1:t){
-		vec[k] <- (1+(ta+mc)/12)*(t-k)
-	}
-	return(vec)
+     vec <- vector(length=t)
+     for(k in 1:t){
+          vec[k] <- (1+(k*(ta+mc)/12))
+     }
+     return(vec)
 }
 
-# Interseccion graficas
-plot(simprod(0.04,0.035,250), col="green", ylim=c(0,20), xlim=c(150,300))
+
+# Simulacion
+plot(simprod(0.04, 0.035, 400), col="green", ylim=c(0,25), xlim=c(0,400), type='l', lwd=2, main="Simulación", ylab='')
 par(new=TRUE)
-plot(simmul(0.04, 0.0933, 250), col="red", ylim=c(0,20), xlim=c(150,300))
-
-
-dife <- simprod(0.04,0.035,300) - simmul(0.04, 0.0933, 300)
-dife[260:300]
-
-
+plot(simmul(0.04, 0.0933, 400), col="red", ylim=c(0,25), xlim=c(0,400), type='l', lwd=2, main="Simulación", ylab='')
